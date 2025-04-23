a25301560d97897cab5824a0096d0a8efca80e91
Nikiforov <rx992.ya.ru>
2025-04-23 22:28:49 +0700

    the first task is completed format

--- src/main/kotlin/taskOne.kt
+++ src/main/kotlin/taskOne.kt
@@ -1,37 +1,33 @@
 fun agoToText(second: Int) {
     val minute = 60
-    val hour =  3600
-    val days =  86400
+    val hour = 3600
+    val days = 86400
 
-    val resultTime = when{
+    val resultTime = when {
         second < minute -> "был(а) только что"
-        second <  hour-> "«был(а) ${second/minute} ${showMinuteText(second)} назад»"
-        second < days -> "«был(а) ${second/hour} ${showHoursText(second)} назад»"
-        second < days*2 -> "«был(а) вчера»"
-        second < days*3 -> "«был(а) позавчера»"
+        second < hour -> "«был(а) ${second / minute} ${showMinuteText(second)} назад»"
+        second < days -> "«был(а) ${second / hour} ${showHoursText(second)} назад»"
+        second < days * 2 -> "«был(а) вчера»"
+        second < days * 3 -> "«был(а) позавчера»"
         else -> "«был(а) давно»"
 
     }
-println(resultTime)
+    println(resultTime)
 }
-fun showMinuteText (second: Int) : String =
-    when (second/60){
-      in 11..14 -> "минут"
-      else -> when ((second/60)% 10){
-          1 -> "минуту"
-          in 2..4 -> "минуты"
-          else -> "минут"
-      }
-
+fun showMinuteText(second: Int): String = when (second / 60) {
+    in 11..14 -> "минут"
+    else -> when ((second / 60) % 10) {
+        1 -> "минуту"
+        in 2..4 -> "минуты"
+        else -> "минут"
     }
 
-
-fun showHoursText (second: Int) : String =
-    when (second/3600){
-        1, 21 -> "час назад"
-        in 5..20 -> "часов назад"
-        else -> "часа назад"
-    }
+}
+fun showHoursText(second: Int): String = when (second / 3600) {
+    1, 21 -> "час назад"
+    in 5..20 -> "часов назад"
+    else -> "часа назад"
+}
 
 
 
