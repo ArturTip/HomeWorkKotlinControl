c0647badad42f55b8fc740600daae5cfb2273282
Nikiforov <rx992.ya.ru>
2025-04-26 19:25:00 +0700

    correcting the phrase back

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,5 +4,8 @@ fun main(){
     agoToText(3800)
     agoToText(86500)*/
 
+    val limitDay = 150_000
+    val limitMonth = 600_000
+
 
 }
---
+++ src/main/kotlin/taskTwo.kt
@@ -0,0 +1,17 @@
+val limitDay = 150_000
+val limitMonth = 600_000
+val limitMonthMasterCard = 75_000
+var amountOfTransfersVisa = 0
+var amountOfTransfersMasterCard = 0
+var amountOfTransfersMasterMir = 0
+
+
+fun calcCommissionVisa(payment:Int ) {
+
+
+}
+fun calcCommissionMastercard(payment:Int, limitMonthMasterCard : Int ) {
+
+
+}
+
